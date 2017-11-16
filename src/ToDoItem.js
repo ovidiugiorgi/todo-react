9a69f1344586d0425295a27aa9ef9cdfe6fd73ed
Ovidiu Giorgi <ovidiugiorgi@gmail.com>
2017-11-17 00:08:53 +0200

    ToDoItem can be checked

--- src/ToDoItem.js
+++ src/ToDoItem.js
@@ -1,21 +1,29 @@
 import React, { Component } from 'react';
 
 class ToDoItem extends Component {
-  constructor({text}) {
+  constructor({ text }) {
     super();
 
-    this.setState({
-      text: text
-    });
+    this.state = {
+      isDone: false,
+      text: text,
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({isDone: event.target.checked});
   }
 
   render() {
     const { text } = this.props;
+    const isDoneClass = this.state.isDone ? 'done' : '';
 
     return (
       <div className="toDoItem">
-        <input type="checkbox"></input>
-        <span>{text}</span>
+        <input type="checkbox" value={this.state.isDone} onChange={this.handleChange}></input>
+        <span className={isDoneClass}>{text}</span>
       </div>
     )
   }
