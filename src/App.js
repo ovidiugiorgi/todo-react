82747e812a19aff35c74aeaf74af6b4982c077c0
Ovidiu Giorgi <ovidiugiorgi@gmail.com>
2017-11-17 01:54:38 +0200

    Extract ToDoForm comp

--- src/App.js
+++ src/App.js
@@ -1,56 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
-import ToDoList from './ToDoList';
-import NewItem from './NewItem';
+
+import ToDoForm from './ToDoForm';
 
 class App extends Component {
-  constructor() {
-    super();
-
-    this.state = {
-      items: [],
-    };
-
-    this.handleDone = this.handleDone.bind(this);    
-    this.handleAdd = this.handleAdd.bind(this);
-  }
-
-  handleAdd(value) {
-    const newItem = {
-      text: value,
-      isDone: false,
-    };
-
-    this.setState({
-      items: this.state.items.concat([newItem]),
-    });
-  }
-
-  handleDone(index, isDone) {
-    let items = this.state.items;
-    let doneItem = items[index];
-    
-    doneItem.isDone = isDone;
-
-    items.splice(index, 1);
-
-    if (isDone) {
-      items.push(doneItem);
-    } else {
-      items.unshift(doneItem);
-    }
-
-    this.setState({
-      items: items,
-    });
-  }
-
   render() {
     return (
       <div>
-        <h1>To Do</h1>
-        <ToDoList items={this.state.items} onDone={this.handleDone}/>
-        <NewItem onAdd={this.handleAdd} />
+        <ToDoForm />
       </div>
     )
   }
---
+++ src/ToDoForm.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import ToDoList from './ToDoList';
+import NewItem from './NewItem';
+
+class ToDoForm extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      items: [],
+    };
+
+    this.handleDone = this.handleDone.bind(this);    
+    this.handleAdd = this.handleAdd.bind(this);
+  }
+
+  handleAdd(value) {
+    const newItem = {
+      text: value,
+      isDone: false,
+    };
+
+    this.setState({
+      items: this.state.items.concat([newItem]),
+    });
+  }
+
+  handleDone(index, isDone) {
+    let items = this.state.items;
+    let doneItem = items[index];
+    
+    doneItem.isDone = isDone;
+
+    items.splice(index, 1);
+
+    if (isDone) {
+      items.push(doneItem);
+    } else {
+      items.unshift(doneItem);
+    }
+
+    this.setState({
+      items: items,
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <ToDoList items={this.state.items} onDone={this.handleDone}/>
+        <NewItem onAdd={this.handleAdd} />
+      </div>
+    )
+  }
+}
+
+export default ToDoForm;
